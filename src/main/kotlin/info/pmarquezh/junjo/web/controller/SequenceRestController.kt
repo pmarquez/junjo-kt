1a2c02a156910cc13bf9d221f13fd7a733f16807
Paulo Márquez Herrero <me@pmarquezh.info>
2023-07-10 12:53:58 +0200

    v0.9.12-SNAPSHOT * Reviewed code in SequenceRestController, many new things learned since last time, code improvements, many more to come.

--- src/main/kotlin/info/pmarquezh/junjo/web/controller/SequenceRestController.kt
+++ src/main/kotlin/info/pmarquezh/junjo/web/controller/SequenceRestController.kt
@@ -1,138 +1,59 @@
 package info.pmarquezh.junjo.web.controller
 
-
-//   Standard Libraries Imports
 import javax.validation.Valid
-
-//   Third Party Libraries Imports
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
-import mu.KotlinLogging
-
-//   FENIX Framework Imports
-
-//   Domain Imports
 import info.pmarquezh.junjo.model.sequence.SequenceDTO
 import info.pmarquezh.junjo.model.sequence.SequenceRec
 import info.pmarquezh.junjo.service.SequenceService
+import org.springframework.validation.annotation.Validated
+import javax.validation.constraints.Min
 
-/**
- * SequenceRestController.kt<br></br><br></br>
- * Creation Date 2022-02-08 16:55<br></br><br></br>
- * **DESCRIPTION:**<br></br><br></br>
- *
- *
- * <PRE>
- * <table width="90%" border="1" cellpadding="3" cellspacing="2">
- * <tr>
- * <th colspan="2">
- * History
-</th> *
-</tr> *
- * <tr>
- * <td width="20%">Version 1.0<br></br>
- * Version Date: 2022-02-08 16:55<br></br>
- * @author pmarquezh
-</td> *
- * <td width="80%">
- *
- * Creation
-</td> *
-</tr> *
-</table> *
-</PRE> *
- * @author pmarquezh
- * @version 1.0 - 2022-02-08 16:55
- */
 @RestController
 @RequestMapping ( "/junjoAPI/1.0/sequences" )
+@Validated
 class SequenceRestController ( private val sequenceService: SequenceService ) {
 
-    private val logger = KotlinLogging.logger { }
-
-    /**
-     * Persists a new Sequence [C].
-     * @return
-     */
     @PostMapping ( path = [""] )
     fun persistSequence ( @Valid @RequestBody sequenceDTO: SequenceDTO ): ResponseEntity<Void> {
-
         val newSequenceId = sequenceService.persistSequence ( sequenceDTO )
-
         val headers = HttpHeaders ( )
             headers.add ( "Location", newSequenceId )
-
         return ResponseEntity ( headers, HttpStatus.CREATED )
-
     }
 
-    /**
-     * Retrieves all Sequences [R].
-     * @return
-     */
     @GetMapping ( path = [""] )
     fun retrieveSequences ( ): ResponseEntity<List<SequenceRec?>?> {
-        val sequences = sequenceService.retrieveSequences ( )
-        return ResponseEntity ( sequences, HttpStatus.OK )
+        return ResponseEntity(sequenceService.retrieveSequences(), HttpStatus.OK)
     }
 
-    /**
-     * Retrieves a Sequence [R].
-     * @return
-     */
     @GetMapping( path = ["/{sequenceId}"])
     fun retrieveSequence(@PathVariable("sequenceId") sequenceId: String?): ResponseEntity<SequenceRec> {
-        val sequence = sequenceService.retrieveSequence(sequenceId)
-        return ResponseEntity(sequence, HttpStatus.OK)
+        return ResponseEntity(sequenceService.retrieveSequence(sequenceId), HttpStatus.OK)
     }
 
-    /**
-     * Updates a Sequence [U].
-     * @return
-     */
     @PutMapping ( path = ["/{sequenceId}" ] )
     fun updateSequence ( @PathVariable("sequenceId") sequenceId: String?, @RequestBody sequenceDTO: SequenceDTO? ): ResponseEntity<Void> {
         sequenceService.updateSequence(sequenceId, sequenceDTO)
         return ResponseEntity(HttpStatus.NO_CONTENT)
     }
 
-    /**
-     * Deletes a Sequence [D].
-     * @return
-     */
     @DeleteMapping( path = ["/{sequenceId}"])
     fun deleteSequence(@PathVariable("sequenceId") sequenceId: String?): ResponseEntity<Void> {
         sequenceService.deleteSequence(sequenceId)
         return ResponseEntity(HttpStatus.NO_CONTENT)
     }
 
-    /**
-     * Generate next element in the sequence.
-     * @return
-     */
     @GetMapping( path = ["/{sequenceId}/generate"])
     fun generateNextElementInSequence ( @PathVariable ( "sequenceId" ) sequenceId: String? ): ResponseEntity<String> {
-        val generatedElement = sequenceService.getNextInSequence(sequenceId)
-        return ResponseEntity(generatedElement, HttpStatus.OK)
+        return ResponseEntity(sequenceService.getNextInSequence(sequenceId), HttpStatus.OK)
     }
 
-    /**
-     * Generate next "n" elements in the sequence.
-     * @return
-     */
     @GetMapping ( path = ["/{sequenceId}/generate/{quantity}"] )
-    fun generateNextElementsInSequence ( @PathVariable("sequenceId") sequenceId: String?, @PathVariable ( "quantity" ) quantity: Int ): ResponseEntity<List<String?>?> {
-//  fun generateNextElementsInSequence ( @PathVariable("sequenceId") sequenceId: String?, @Valid @Min ( 1 ) @PathVariable ( "quantity" ) quantity: Int ): ResponseEntity<List<String?>?> {
-
-        //   JACK SPARROW WAS HERE BIG TIME!!! @PathVariable validations NOT working. OPEN A TICKET WITH THIS ISSUE RIGHT AWAY
-        if ( quantity < 0 ) { return ResponseEntity ( HttpStatus.BAD_REQUEST ) }
-
-        val generatedElements = sequenceService.getNextElementsInSequence ( sequenceId, quantity )
-
-        return ResponseEntity ( generatedElements, HttpStatus.OK )
-
+  fun generateNextElementsInSequence ( @PathVariable("sequenceId") sequenceId: String, @PathVariable ( "quantity" ) @Valid @Min( 1 ) quantity: Int ): ResponseEntity<List<String?>?> {
+        return ResponseEntity (sequenceService.getNextElementsInSequence ( sequenceId, quantity ), HttpStatus.OK)
     }
 
 }
